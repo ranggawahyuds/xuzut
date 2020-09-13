d4ea9ca15282346ad5e865b794a63d8edbe548bc
ranggawahyuds <56479538+ranggawahyuds@users.noreply.github.com>
2020-09-13 23:33:50 +0700

    Add files via upload

---
+++ auto.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+
+clear
+echo "\033[1;32m        START "
+sleep 1s
+
+ulang="y"
+
+while [ $ulang = "y" ]
+do
+
+python jake.py doge
+
+x=3
+while [ $x -gt 0 ]
+do
+sleep 1s
+clear
+echo " \033[1;32m Mulai ulang sisa Waktu anda $x Detik"
+x=$(( $x - 1 ))
+done
+
+
+
+done
